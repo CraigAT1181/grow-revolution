c8032b5ac111230eef0ea11b315f8df14a1342e3
craig <craigtipple81@gmail.com>
2024-12-26 15:05:26 +0000

    Added tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <div>
+    <div className="bg-green-300">
       <i className="fa-solid fa-spinner fa-spin"></i>
 
       <p>Hello!</p>

c6aef44a82a45412f5d3d40c354e969bbe77df7d
craig <craigtipple81@gmail.com>
2024-12-26 15:22:38 +0000

    Updated tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import "./App.css";
-import "./index.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-function App() {
-  return (
-    <div className="bg-green-300">
-      <i className="fa-solid fa-spinner fa-spin"></i>
-
-      <p>Hello!</p>
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
